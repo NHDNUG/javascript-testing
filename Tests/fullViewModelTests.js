35c0637954ca2a2826ea625177c14cef3a33b34a
Kevin Cavnar-Johnson <kevin.cavnar-johnson@improvingenterprises.com>
2016-04-20 22:24:40 -0500

    Slight modification to view model tests

--- Tests/fullViewModelTests.js
+++ Tests/fullViewModelTests.js
@@ -1,6 +1,6 @@
 define(['squire', 'sinon'], function(Squire, sinon) {
   var injector = new Squire();
-  var Header, Requests;
+  var viewModel;
   //We use squire to inject our own mocked versions of RequireJS dependencies.  
   injector.mock({
     'cookie-reader': {
@@ -19,13 +19,8 @@ define(['squire', 'sinon'], function(Squire, sinon) {
 
     beforeEach( function ( done ) {
       //This require block behaves the same way as a normal RequireJS block but uses our mocked dependencies instead 
-      injector.mock({
-        'cookieReader': {
-          userName: sinon.stub().returns("Test User Name"),
-        }
-      });
 
-      injector.require( ["full-view-model"], function ( header ) {
+      injector.require( ["full-view-model"], function ( ViewModel ) {
 
 
         //If you want to verify that a request is made but don't care about handling the response, you can uncomment this instead of using sinon's fake Server
@@ -38,7 +33,7 @@ define(['squire', 'sinon'], function(Squire, sinon) {
 
         //This sets up a fake server that will intercept any XMLHTTPRequests and can respond to them
         this.server = sinon.fakeServer.create();
-        Header = new header()
+        viewModel = new ViewModel()
         done();
       } );
 
@@ -46,12 +41,12 @@ define(['squire', 'sinon'], function(Squire, sinon) {
 
     describe("Full View Model", function() {
       it( "should initialize the cart with zero items", function () {
-        Header.cartItemCount().should.be.exactly(0).and.be.a.Number;
+        viewModel.cartItemCount().should.be.exactly(0).and.be.a.Number;
       });
 
       //Note that by passing in a done method, this becomes an async test (but make sure you call it or it will wait forever)
       it("should return the user name from the external dependency", function(done) {
-        Header.name().should.be.exactly("Test User Name");
+        viewModel.name().should.be.exactly("Test User Name");
         done();
       } );
 
@@ -61,7 +56,7 @@ define(['squire', 'sinon'], function(Squire, sinon) {
         server.respondWith( "/CartService", [200, { "Content-Type": "application/json" }, responseData] );
         server.respond();
 
-        Header.cartItemCount().should.be.exactly(5).and.be.a.Number;
+        viewModel.cartItemCount().should.be.exactly(5).and.be.a.Number;
         done();
       })
     })
